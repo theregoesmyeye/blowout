900c91a5e60daf6576f20eaa62e726f6cef4bcee
theregoesmyeye <64832453+theregoesmyeye@users.noreply.github.com>
2024-05-03 04:51:43 -0400

    Update install_required_stuff.sh
    Now it installs the wordlist for you

--- install_required_stuff.sh
+++ install_required_stuff.sh
@@ -1,3 +1,9 @@
-pkg install -y clang python libffi openssl libsodium
-SODIUM_INSTALL=system pip install pynacl
-pip install paramiko
+pkg install clang python libffi openssl libsodium 
+SODIUM_INSTALL=system pip install pynacl 
+pip install paramiko 
+git clone https://github.com/zacheller/rockyou 
+cd rockyou 
+tar xvf rockyou.txt.tar.gz 
+mv rockyou.txt .. 
+rm -rf rockyou 
+echo "Wordlist has been added. Thanks to https://github.com/zacheller for providing the mirror."
